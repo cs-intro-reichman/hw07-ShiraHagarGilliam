bc53a9192daaacc8eaeecb62981d304ad59548a3
Shira Gilliam <154455888+ShiraHagarGilliam@users.noreply.github.com>
2024-01-31 17:43:06 +0200

    Hw7

--- SpellChecker.java
+++ SpellChecker.java
@@ -6,30 +6,54 @@ public class SpellChecker {
 		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
-		String correction = spellChecker(word, threshold, dictionary);
+		String correction = spellChecker("Hell9", 3, dictionary);
 		System.out.println(correction);
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
-	}
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		if(word2.length() == 0) {
+			return word1.length();
+		}
+		if (word1.length() == 0) {
+			return word2.length();
+		}
+		if(word1.charAt(0) == word2.charAt(0)) {
+			return levenshtein(tail(word1), tail(word2));
+		} 
+		int firstMinimum = Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2)));
+		return 1 + Math.min(firstMinimum, levenshtein(tail(word1), tail(word2)));
+		}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
+		int i = 0;
 		In in = new In(fileName);
 
-		// Your code here
-
+		while(!in.isEmpty()) {
+			dictionary[i] = in.readLine();
+			i++;
+		}
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int stepsCount = threshold + 1;
+		String newWord = "";
+		for(int i = 0; i < dictionary.length; i++) {
+			if(stepsCount > levenshtein(word, dictionary[i])) {
+                stepsCount = levenshtein(word, dictionary[i]);
+				newWord = dictionary[i];
+			}
+		}
+		if(stepsCount > threshold) {
+			return word;
+		}
+		return newWord;
 	}
-
 }
